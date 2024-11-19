653cc64ed117908db6d8b4ec6b7dcad192bfa681
rinku09090909 <nater79208@walshun.com>
2024-11-19 13:59:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,11 +38,11 @@ def install_requirements():
 install_requirements()
 
 # 🎛️ Telegram API token (replace with your actual token)
-TOKEN = '7424479555:AAGtNAbqXax_b3b--QnGYi5_XwBbQNw'
+TOKEN = '7946707301:AAFHG-r4ail939ZTKBHUR24FxWE0VBoAsyQ'
 bot = telebot.TeleBot(TOKEN, threaded=False)
 
 # 🛡️ List of authorized user IDs (replace with actual IDs)
-AUTHORIZED_USERS = [5588464519]
+AUTHORIZED_USERS = [643217421991]
 
 # 🌐 Global dictionary to keep track of user attacks
 user_attacks = {}
